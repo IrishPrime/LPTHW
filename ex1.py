5652f607ad32102657601b2aa4dab7fc72bdc2b8
Michael O'Neill <irish.dot@gmail.com>
2015-02-13 20:05:21 -0500

    Complete Exercise 1: A Good First Program

---
+++ ex1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Learn Python the Hard Way - Exercise 1"""
+
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
