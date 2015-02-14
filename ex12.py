d4d8a9e1a871c33ae8404948394d4ff06ccd6021
Michael O'Neill <irish.dot@gmail.com>
2015-02-14 00:09:42 -0500

    Complete Exercise 12: Prompting People

---
+++ ex12.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Learn Python the Hard Way - Exercise 12"""
+
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+print("So, you're %r old, %r tall and %r heavy." % (age, height, weight))
