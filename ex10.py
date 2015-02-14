11e60f42fdcab92522ca53f55891fc50fb2bf226
Michael O'Neill <irish.dot@gmail.com>
2015-02-13 23:37:33 -0500

    Complete Exercise 10: What Was That?

---
+++ ex10.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Learn Python the Hard Way - Exercise 10"""
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
