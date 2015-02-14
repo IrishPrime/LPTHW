6102381f94b4cad2189d3f625637ff31e873d9ff
Michael O'Neill <irish.dot@gmail.com>
2015-02-13 23:56:51 -0500

    Complete Exercise 11: Asking Questions

---
+++ ex11.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Learn Python the Hard Way - Exercise 11"""
+
+print("How old are you? ", end="")
+age = input()
+print("How tall are you? ", end="")
+height = input()
+print("How much do you weigh? ", end="")
+weight = input()
+
+print("So, you're %r old, %r tall and %r heavy." % (age, height, weight))
