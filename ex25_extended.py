0eba10eec72e497d564ee95f813b64bf241d81eb
Michael O'Neill <irish.dot@gmail.com>
2015-02-20 23:29:02 -0500

    Complete Exercise 25: Even More Practice

--- ex25_extended.py
+++ ex25_extended.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
 """Learn Python the Hard Way - Exercise 25"""
 
-import ex25
+from ex25 import *
 
 sentence = "All good things come to those who wait."
-words = ex25.break_words(sentence)
+words = break_words(sentence)
 print(words)
-sorted_words = ex25.sort_words(words)
+sorted_words = sort_words(words)
 print(sorted_words)
-ex25.print_first_word(words)
-ex25.print_last_word(words)
+print_first_word(words)
+print_last_word(words)
 print(words)
-ex25.print_first_word(sorted_words)
-ex25.print_last_word(sorted_words)
+print_first_word(sorted_words)
+print_last_word(sorted_words)
 print(sorted_words)
-sorted_words = ex25.sort_sentence(sentence)
+sorted_words = sort_sentence(sentence)
 print(sorted_words)
-ex25.print_first_and_last(sentence)
-ex25.print_first_and_last_sorted(sentence)
+print_first_and_last(sentence)
+print_first_and_last_sorted(sentence)
